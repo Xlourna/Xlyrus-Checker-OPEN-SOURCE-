dc42d5443f241352f9c06d051644b22fc5e1f0b4
Xlourna <90231636+Xlourna@users.noreply.github.com>
2021-11-07 02:16:15 -0600

    Add files via upload

---
+++ Xlyrus Checker (OPEN SOURCE).py
@@ -0,0 +1,234 @@
+import discord
+from discord.ext import commands
+import random
+import os
+ 
+
+os.system('cls')
+token = input("Enter Token: ")
+client = commands.Bot(command_prefix=commands.when_mentioned_or("$"))
+os.system('cls')
+os.system(f'title [XLYRUS AFK CHECKER]')
+os.system(f'mode 60,20')
+ 
+a = '1'
+b = '2'
+c = '3'
+d = '4'
+e = '5'
+f = '6'
+g = '7'
+h = '8'
+i = '9'
+j = '10'
+k = '11'
+l = '12'
+m = '13'
+n = '14'
+o = '15'
+p = '16'
+q = '17'
+r = '18'
+s = '19'
+t = '20'
+u = '21'
+v = '22'
+w = '23'
+x = '24'
+y = '25'
+z = '26'
+aa = '27'
+bb = '28'
+cc = '29'
+dd = '30'
+ee = '31'
+ff = '32'
+gg = '33'
+hh = '34'
+ii = '35'
+jj = '36'
+kk = '37'
+ll = '38'
+mm = '39'
+nn = '40'
+oo = '41'
+pp = '42'
+qq = '43'
+rr = '44'
+ss = '45'
+tt = '46'
+uu = '47'
+vv = '48'
+ww = '49'
+xx = '50'
+yy = '51'
+zz = '52'
+aaa = '53'
+bbb = '54'
+ccc = '55'
+ddd = '56'
+eee = '57'
+fff = '58'
+ggg = '59'
+hhh = '60'
+iii = '61'
+jjj = '62'
+kkk = '63'
+lll = '64'
+mmm = '65'
+nnn = '66'
+ooo = '67'
+ppp = '68'
+qqq = '69'
+rrr = '70'
+sss = '71'
+ttt = '72'
+vvv = '73'
+www = '74'
+xxx = '75'
+yyy = '76'
+zzz = '77'
+aaaa = '78' 
+bbbb = '79'
+cccc = '80'
+dddd = '81'
+eeee = '82'
+ffff = '83'
+gggg = '84'
+hhhh = '85'
+iiii = '86'
+jjjj = '87'
+kkkk = '88'
+llll = '89'
+mmmm = '90'
+nnnn = '91'
+oooo = '92'
+pppp = '93'
+qqqq = '94'
+rrrr = '95'
+ssss = '96'
+tttt = '97'
+uuuu = '98'
+vvvv = '99'
+wwww = '100'
+@client.event
+async def on_ready():
+  print(f""" \u001b[31m
+ ------------------------------------------------------------------------------------
+  say "afk check (tag)" to afkcheck the mentioned user, and it'll count for you.
+ ╔══════════════════════════════╗
+  ╔═╗╦ ╦╔╦╗╔═╗╔═╗╦ ╦╔═╗╦╔═╔═╗╦═╗
+  ╠═╣║ ║ ║ ║ ║║  ╠═╣║╣ ╠╩╗║╣ ╠╦╝
+  ╩ ╩╚═╝ ╩ ╚═╝╚═╝╩ ╩╚═╝╩ ╩╚═╝╩╚═
+ ╚══════════════════════════════╝
+ made by 5tacey#8308 & yy#3000 join discord.gg/chiefjustice
+-------------------------------------------------------------------------------------
+""")
+ 
+@client.event
+async def on_message(message):
+    channel = message.channel
+    if message.content.startswith('afk check'):
+        await message.channel.send(a)
+        await message.channel.send(b)
+        await message.channel.send(c)
+        await message.channel.send(d)
+        await message.channel.send(e)
+        await message.channel.send(f)
+        await message.channel.send(g)
+        await message.channel.send(h)
+        await message.channel.send(i)
+        await message.channel.send(j)  
+        await message.channel.send(k)  
+        await message.channel.send(l)  
+        await message.channel.send(m)  
+        await message.channel.send(n)  
+        await message.channel.send(o) 
+        await message.channel.send(p)
+        await message.channel.send(q) 
+        await message.channel.send(r)
+        await message.channel.send(s)
+        await message.channel.send(t)  
+        await message.channel.send(u)
+        await message.channel.send(v)
+        await message.channel.send(w)  
+        await message.channel.send(x)
+        await message.channel.send(y)
+        await message.channel.send(z)
+        await message.channel.send(aa)
+        await message.channel.send(bb)
+        await message.channel.send(cc)
+        await message.channel.send(dd)
+        await message.channel.send(ee)
+        await message.channel.send(ff)
+        await message.channel.send(gg)
+        await message.channel.send(hh)
+        await message.channel.send(ii)
+        await message.channel.send(jj)  
+        await message.channel.send(kk)  
+        await message.channel.send(ll)  
+        await message.channel.send(mm)  
+        await message.channel.send(nn)  
+        await message.channel.send(oo) 
+        await message.channel.send(pp)
+        await message.channel.send(qq)  
+        await message.channel.send(rr)
+        await message.channel.send(ss)
+        await message.channel.send(tt)  
+        await message.channel.send(uu)
+        await message.channel.send(vv)
+        await message.channel.send(ww)  
+        await message.channel.send(xx)
+        await message.channel.send(yy)
+        await message.channel.send(zz)
+        await message.channel.send(aaa)
+        await message.channel.send(bbb)
+        await message.channel.send(ccc)
+        await message.channel.send(ddd)
+        await message.channel.send(eee)
+        await message.channel.send(fff)
+        await message.channel.send(ggg)
+        await message.channel.send(hhh)
+        await message.channel.send(iii)
+        await message.channel.send(jjj)  
+        await message.channel.send(kkk)  
+        await message.channel.send(lll)  
+        await message.channel.send(mmm)  
+        await message.channel.send(nnn)  
+        await message.channel.send(ooo) 
+        await message.channel.send(ppp)
+        await message.channel.send(qqq)  
+        await message.channel.send(rrr)
+        await message.channel.send(sss)
+        await message.channel.send(ttt)  
+        await message.channel.send(vvv)
+        await message.channel.send(www)  
+        await message.channel.send(xxx)
+        await message.channel.send(yyy)
+        await message.channel.send(zzz)
+        await message.channel.send(aaaa)
+        await message.channel.send(bbbb)
+        await message.channel.send(cccc)
+        await message.channel.send(dddd)
+        await message.channel.send(eeee)
+        await message.channel.send(ffff)
+        await message.channel.send(gggg)
+        await message.channel.send(hhhh)
+        await message.channel.send(iiii)
+        await message.channel.send(jjjj)  
+        await message.channel.send(kkkk)  
+        await message.channel.send(llll)  
+        await message.channel.send(mmmm)  
+        await message.channel.send(nnnn)  
+        await message.channel.send(oooo) 
+        await message.channel.send(pppp)
+        await message.channel.send(qqqq)  
+        await message.channel.send(rrrr)
+        await message.channel.send(ssss)
+        await message.channel.send(tttt)  
+        await message.channel.send(uuuu)
+        await message.channel.send(vvvv)
+        await message.channel.send(wwww) 
+
+
+client.run(token, bot=False)
